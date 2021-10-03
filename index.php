4c493932af8dc6142bbc005a3692d24dc4c10336
Amir Hossein Habibi <habibi.dev@gmail.com>
2021-10-03 10:23:29 +0330

    init

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Amir Hossein Habibi">
+    <meta name="generator" content="Amir Hossein Habibi">
+
+    <title>در حال بروز رسانی</title>
+
+    <link href="style.css" rel="stylesheet">
+    <link rel="icon" href="images/reload.png" type="image/png">
+
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title">
+                <img src="images/reload.png" title="در حال بروز رسانی..." alt="در حال بروز رسانی...">
+                <br>
+                سامانه در حال بروز رسانی می باشد.
+                <br>
+                <span>
+                    لطفا ساعاتی دیگر مجدد مراجعه کنید.
+                </span>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
